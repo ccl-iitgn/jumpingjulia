e9aea9689a2f75afb688daf8e52862027970de70
bhanupattemz <bhanupattemz@gmail.com>
2025-06-27 12:31:16 +0530

    update font size

--- src/Download.jsx
+++ src/Download.jsx
@@ -3,35 +3,8 @@ import { MdCancel } from "react-icons/md";
 
 import html2canvas from 'html2canvas';
 import jsPDF from 'jspdf';
+import {colors} from "./Colors"
 
-let colors = {
-    0: "#B3C7F7",
-    1: "#F7D29C",
-    2: "#CAB8F2",
-    3: "#F2B8A2",
-    4: "#A7D3F5",
-    5: "#F6C3DC",
-    6: "#D7BCE8",
-    7: "#F2CB8E",
-    8: "#B0A4E3",
-    9: "#FFCF9F",
-    10: "#C6D2FA",
-    11: "#F7C4A5",
-    12: "#C9B4ED",
-    13: "#FAD6A5",
-    14: "#BFD6F8",
-    15: "#E5B9F0",
-    16: "#D0B4E3",
-    17: "#FBCFAA",
-    18: "#AFC6F7",
-    19: "#E3AEE3",
-    20: "#F3BC8D",
-    21: "#C9B0FA",
-    22: "#F6BFA0",
-    23: "#ADBCF9",
-    24: "#E2A3DB",
-    25: "#F2AE8D"
-};
 
 function MultiGameComponent({ games, heading, open, setOpen }) {
     const screenWidth = window.innerWidth
